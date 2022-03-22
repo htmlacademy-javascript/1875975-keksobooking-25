efe37c4a968c27cedf10ee38db1f6aad9b20b51f
Keks <keks@htmlacademy.ru>
2022-03-22 21:43:07 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,10 @@
+import {createAds} from './data.js';
+import './card.js';
+import {getDisableForm, getActiveForm} from './form.js';
+import './form-validator.js';
+
+getDisableForm();
+
+createAds();
+
+getActiveForm();
