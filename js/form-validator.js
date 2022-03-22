ab03bc00820651d60010ca2c42711477a21f5714
cinis-ad-cinis <valentinakhramtsova@gmail.com>
2022-03-22 21:40:32 +0300

    Вторая часть валидации

--- js/form-validator.js
+++ js/form-validator.js
@@ -1,4 +1,27 @@
 const form = document.querySelector('.ad-form');
+const title = form.querySelector('#title');
+const price = form.querySelector('#price');
+const type = form.querySelector('#type');
+const time = form.querySelector('.ad-form__element--time');
+const checkIn = form.querySelector('#timein');
+const checkOut = form.querySelector('#timeout');
+const rooms = form.querySelector('#room_number');
+const guests = form.querySelector('#capacity');
+const MIN_PRICE = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+const MAX_PRICE = 100000;
+
+const ROOMS_OPTIONS = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0'],
+};
 
 const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
@@ -7,32 +30,48 @@ const pristine = new Pristine(form, {
   errorTextClass: 'ad-form__error-text',
 });
 
-const title = form.querySelector('#title');
+//Заголовок
 const validateTitle = (value) => value.length >= 30 && value.length <= 100;
+
 pristine.addValidator (
   title,
   validateTitle,
   'От 30 до 100 символов', 2, true,
 );
 
-const price = form.querySelector('#price');
-const validatePrice = (value) => value > 0 && value <= 100000;
+// Цена и тип жилья
+const validatePrice = (value) => value >= MIN_PRICE[type.value] && value <= MAX_PRICE;
+const getPriceErrorMessage = () => `Цена от ${MIN_PRICE[type.value]} до ${MAX_PRICE} ₽/ночь`;
+
 pristine.addValidator (
   price,
   validatePrice,
-  'Максимальная цена 100 000 ₽/ночь',
+  getPriceErrorMessage
 );
 
-const rooms = form.querySelector('#room_number');
-const guests = form.querySelector('#capacity');
-const roomsOption = {
-  '1': ['1'],
-  '2': ['1', '2'],
-  '3': ['1', '2', '3'],
-  '100': ['0'],
+const onTypeChange = () => {
+  price.placeholder = MIN_PRICE[type.value];
+  price.min = MIN_PRICE[type.value];
+  pristine.validate(price);
 };
 
-const validateRoomsGuests = () => roomsOption[rooms.value].includes(guests.value);
+type.addEventListener('change', () => {
+  onTypeChange();
+});
+
+// Время заезда и выезда
+
+const onTimeChange = (evt) => {
+  checkIn.value = evt.target.value;
+  checkOut.value = evt.target.value;
+};
+
+time.addEventListener('change', (evt) => {
+  onTimeChange(evt);
+});
+
+// Количество комнат и мест
+const validateRoomsGuests = () => ROOMS_OPTIONS[rooms.value].includes(guests.value);
 
 const getRoomsGuestsErrorMessage = () => {
   switch (rooms.value) {
@@ -51,10 +90,12 @@ rooms.addEventListener('change', () => {
   pristine.validate(guests);
 });
 
-pristine.addValidator(guests, validateRoomsGuests, getRoomsGuestsErrorMessage);
+pristine.addValidator(
+  guests,
+  validateRoomsGuests,
+  getRoomsGuestsErrorMessage);
 
 form.addEventListener('submit', (evt) => {
-
   const isValid = pristine.validate();
   if (!isValid) {
     evt.preventDefault();
